52fe55e6db095ab433f96819212128586c5275b9
Sebastian Good <2230835+scagood@users.noreply.github.com>
2023-08-15 17:38:57 +0300

    chore: Update some deps

--- src/atom.d.ts
+++ src/atom.d.ts
@@ -1,10 +1,20 @@
-import type { Package, PackageManager } from 'atom'
+import type { PackageJson } from 'type-fest'
 
+import { Dependency } from './types'
 // TODO upstream these changes to @types/atom
+
+interface PackageMetadata extends PackageJson {
+  "package-deps"?: (string | Dependency | Dependency[])[]
+}
+
+// "package-deps": [ "linter" ]
+// "package-deps": [ { "name": "linter" } ]
+// "package-deps": [ { "name": "linter", "minimumVersion": "2.0.0" } ],
+// "package-deps": [ [ { "name": "linter" }, { "name": "atom-ide-ui" } ] ]
 
 declare module 'atom/src/package' {
   interface Package {
-    metadata: Record<string, any>
+    metadata: PackageMetadata
   }
 }
 
